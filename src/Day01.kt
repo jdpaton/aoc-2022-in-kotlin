b3b02506e239b947ec4e05063d58a8b16c7dd910
Jamie Paton <jpaton@ea.com>
2022-12-15 21:07:59 -0800

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,49 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1() {
+        val testInput = readInput("Day01_test")
+
+        var elfIndex = 1
+        var currentTotal = 0
+        val elfMap = mutableMapOf<Int, Int>()
+        testInput.forEach { line ->
+            if (line.isNotEmpty()) {
+                currentTotal += line.toInt()
+            } else {
+                elfMap[elfIndex] = currentTotal
+                currentTotal = 0
+                elfIndex += 1
+            }
+        }
+        val biggestElfIdx = elfMap.maxBy { it.value }
+        println(biggestElfIdx)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2() {
+        val testInput = readInput("Day01_test")
+
+        var elfIndex = 1
+        var currentTotal = 0
+        val elfMap = mutableMapOf<Int, Int>()
+        testInput.forEach { line ->
+            if (line.isNotEmpty()) {
+                currentTotal += line.toInt()
+            } else {
+                elfMap[elfIndex] = currentTotal
+                currentTotal = 0
+                elfIndex += 1
+            }
+        }
+
+        val topThree = mutableSetOf<Pair<Int,Int>>()
+        for(i in 1..3) {
+            val elf = elfMap.maxBy { it.value }
+            topThree.add(elf.toPair())
+            elfMap.remove(elf.key)
+        }
+        println(topThree.sumOf { it.second })
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    part1()
+    part2()
 }
